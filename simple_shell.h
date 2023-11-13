4079fb1b5e11e1b95e2faee0b5809770e2dd3748
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-11-13 17:53:35 +0000

    shell.h file

---
+++ simple_shell.h
@@ -0,0 +1,16 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+/*HEADERS*/
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+/*PROTOTYPES*/
+
+
+#endif
