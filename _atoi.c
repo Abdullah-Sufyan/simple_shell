b54e84a94aa4f7925a8df6ca26be70c679dd7890
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-11-16 11:11:36 -0800

    update

--- _atoi.c
+++ _atoi.c
@@ -1,12 +1,25 @@
 #include "simple_shell.h"
 
+/**
+ * _atoi - Convert a string to an integer.
+ * @inputString: The string containing the numeric characters.
+ *
+ * Return: The converted integer value.
+ */
+
 int _atoi(const char *inputString)
 {
 	int result = 0, sign = 1;
 
-	while (_isspace(*inputString)) inputString++;
-	if (*inputString == '-') { sign = -1; inputString++; }
-	else if (*inputString == '+') inputString++;
+	while (_isspace(*inputString))
+		inputString++;
+	if (*inputString == '-')
+	{
+		sign = -1;
+		inputString++;
+	}
+	else if (*inputString == '+')
+		inputString++;
 
 	while (_isdigit(*inputString))
 	{
