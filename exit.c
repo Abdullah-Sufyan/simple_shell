a0262dae013385f59e201d5d3201c9b7946a8d82
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-11-17 16:20:55 -0800

    update

--- exit.c
+++ exit.c
@@ -1,60 +1,75 @@
 #include "simple_shell.h"
 
+/**
+ * executeCommand - Forks and executes a given command
+ * @command: Command to be executed
+ */
 void executeCommand(char *command)
 {
 	pid_t pid = fork();
 
-    if (pid < 0)
-    {
-	    perror("FORK FAILED");
-	    exit(1);
-    }
-    else if (pid == 0)
-    {
-	    char *args[2];
-	    args[0] = command;
-	    args[1] = NULL;
+	if (pid < 0)
+	{
+		perror("FORK FAILED");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		char *args[2];
 
-	    if (execve(command, args, NULL) == -1)
-	    {
-		    perror("execve");
-		    exit(1);
-	    }
-    }
-    else
-    {
-	    int status;
-	    waitpid(pid, &status, 0);
-    }
+		args[0] = command;
+
+		args[1] = NULL;
+
+		if (execve(command, args, NULL) == -1)
+		{
+			perror("execve");
+			exit(1);
+		}
+	}
+	else
+	{
+		int status;
+
+		waitpid(pid, &status, 0);
+
+	}
 }
 
+/**
+ * handleUserInput - Handles user input and executes commands
+ */
 void handleUserInput(void)
 {
 	char command[MAX_CMD_LENGTH];
 	char *newline;
 
-    while (1)
-    {
-	    _printf("simple_shell$ ");
-	    fflush(stdout);
+	while (1)
+	{
+		_printf("simple_shell$ ");
+		fflush(stdout);
 
-	    if (_getf(command, MAX_CMD_LENGTH, stdin) == NULL)
-	    {
-		    _printf("\nExiting simple shell...\n");
-		    break;
-	    }
+		if (_getf(command, MAX_CMD_LENGTH, stdin) == NULL)
+		{
+			_printf("\nExiting simple shell...\n");
+			break;
+		}
 
-	    newline = strtok(command, "\n");
+		newline = strtok(command, "\n");
 
-	    if (newline == NULL || _strcmp(newline, "exit") == 0)
-	    {
-		    break;
-	    }
+		if (newline == NULL || _strcmp(newline, "exit") == 0)
+		{
+			break;
+		}
 
-	    executeCommand(newline);
-    }
+		executeCommand(newline);
+	}
 }
 
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
 int main(void)
 {
 	handleUserInput();
